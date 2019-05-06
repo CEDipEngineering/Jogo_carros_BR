c07c6e672abe9a430df421bce9a6c635835a3288
Carlos Dip <cadudip@hotmail.com>
2019-05-06 12:03:30 -0300

    Criada classe do carrinho. Moventação com física implementada. Sprite temporário.

---
+++ player.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  6 11:07:56 2019
+
+@author: Carlos Dip
+"""
+import pygame
+from os import path
+
+pygame.init()
+
+img_dir = path.join(path.dirname(__file__), 'Assets')
+
+WIDTH = 480 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+class Player (pygame.sprite.Sprite):
+    
+    def __init__(self):
+        
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load(path.join(img_dir, "Temp_car_sprite.png")).convert()
+        self.rect = self.image.get_rect()
+        self.size = 15
+        self.acc = 0
+        self.speed = 0
+        self.power = 0.2
+        self.SpeedLimit = 5
+        self.xpos = WIDTH / 2
+        self.rect.centerx = self.xpos
+        self.rect.bottom = HEIGHT - self.size
+
+        
+    def update(self):
+        # Physics
+        
+        if self.speed <= self.SpeedLimit:
+            self.speed += self.acc
+        else:
+            self.speed = self.SpeedLimit
+        
+        self.xpos += self.speed
+        
+        
+        # Position
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 15
+        self.rect.centerx = self.xpos
+        
+        #Keep in screen (Crash into walls)
+        if self.xpos >= WIDTH:
+            self.xpos = WIDTH
+            self.speed = 0
+            self.acc = 0
+        if self.xpos <= 0:
+            self.xpos = 0
+            self.speed = 0
+            self.acc = 0
+        
+        # Friction -- BETA
+#        if self.acc>0:
+#            self.acc-=0.9
+#        elif self.acc<0:
+#            self.acc+=0.9
+#        else:
+#            self.acc = 0
+        
+player = Player()
+clock = pygame.time.Clock()
+#background = pygame.Rect(0,0,WIDTH,HEIGHT)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+try:
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            
+            # Verifica se foi fechado
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.acc -= player.power
+                if event.key == pygame.K_RIGHT:
+                    player.acc += player.power
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.acc = 0
+                if event.key == pygame.K_RIGHT:
+                    player.acc = 0
+                    
+        all_sprites.update()
+        
+        screen.fill((87,87,87))
+#        screen.blit(background)
+        
+        all_sprites.draw(screen)
+        
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+finally:
+    pygame.quit()
