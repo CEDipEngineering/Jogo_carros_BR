cfda4873173787c98973161eb7c7b47cf7528d4d
AndreLopesENG <lopesandre450@gmail.com>
2019-05-07 16:46:49 -0300

    modificações tirando o img_dir

--- consts.py
+++ consts.py
@@ -4,7 +4,7 @@ Created on Tue May  7 16:01:00 2019
 
 @author: Carlos Dip
 """
-from os import path
+
 import json 
 
 WIDTH = 800
@@ -16,13 +16,13 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-img_dir = path.join(path.dirname(__file__), 'Assets\img')
+
 
 consts = {'WIDTH': WIDTH, 'HEIGHT': HEIGHT,
           'FPS': FPS, 'WHITE': WHITE,
           'BLACK': BLACK, 'RED': RED,
           'GREEN': GREEN, 'BLUE': BLUE,
-          'YELLOW': YELLOW, 'img_dir': img_dir}
+          'YELLOW': YELLOW}
 
 
 with open('consts.txt','w') as consts_txt:
