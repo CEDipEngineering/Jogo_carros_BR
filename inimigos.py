6cb502830ea999412690ec59e51910b25007f3ae
AndreLopesENG <lopesandre450@gmail.com>
2019-05-24 09:32:51 -0300

    merge

--- inimigos.py
+++ inimigos.py
@@ -7,15 +7,9 @@ Created on Mon May  6 22:00:55 2019
 
 import pygame
 import random
-import json
+from consts import WIDTH
+from consts import HEIGHT
 
-with open('consts.txt', 'r') as consts_txt:
-    conteudo = consts_txt.read()
-    consts = json.loads(conteudo)
-
-FPS = consts['FPS']
-HEIGHT = consts['HEIGHT']
-WIDTH = consts['WIDTH']
 
 class Inimigo(pygame.sprite.Sprite):
     
@@ -26,27 +20,21 @@ class Inimigo(pygame.sprite.Sprite):
         self.image = img
         self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
-        self.radius = int(self.rect.width * 0.85 / 2)
+        self.radius = int(self.rect.width * 0.35 / 2)
         self.speedy = 0
         self.boss = boss
-        self.speedx = 5
+        self.speedx = 0
         self.size = size
         self.col = col
         self.rect.centerx = col * (WIDTH/5)-self.rect.width
-        self.rect.top = random.randint(-400,-200)
+        self.rect.top = random.randint(-300,-100)
         self.HP = 1
-        if self.boss:
-            self.rect.centerx = WIDTH/2
-            self.rect.top = 5
     def update(self):
         if self.HP <=0:
             self.kill()
             self.boss = False
         if self.boss:
-            if self.rect.x <= 0 or self.rect.x >= WIDTH:
-                self.speedx *= -1
-                self.speedx = random.randint(2,7)
-            self.rect.x += self.speedx    
+            pass
         pass
     
     def updateSpeed(self, speedy):
@@ -54,10 +42,6 @@ class Inimigo(pygame.sprite.Sprite):
             self.rect.top += speedy
         else:
             pass
-    
-    def Bossfire(self, bullet):
-        bullet.yspeed *= -1
-        
             
         
         
