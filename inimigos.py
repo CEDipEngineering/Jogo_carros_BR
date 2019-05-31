41b974279f58c6cc8d1b7dfc9a645dcd79203338
Carlos Dip <cadudip@hotmail.com>
2019-05-31 09:03:31 -0300

    Fixed minor bugs in main. Started cleaning up inimigos.

--- inimigos.py
+++ inimigos.py
@@ -9,10 +9,13 @@ import pygame
 import random
 import json
 
+
+## IMPORTING FILE FROM CONSTS AND RECREATING THE DICTIONARY WITH IMPORTANT VARIABLES 
 with open('consts.txt', 'r') as consts_txt:
     conteudo = consts_txt.read()
     consts = json.loads(conteudo)
 
+## RENAMING VARIABLES LOCALLY
 FPS = consts['FPS']
 HEIGHT = consts['HEIGHT']
 WIDTH = consts['WIDTH']
@@ -20,31 +23,50 @@ WIDTH_STREET = consts['WIDTH_STREET']
 BLACK = consts['BLACK']
 RED = consts['RED']
 
-
+## ENEMY CLASS
 class Inimigo(pygame.sprite.Sprite):
     
-    # Construtor da classe.
     def __init__(self, img, col = 1, size = 15, boss = False):
+        
+        ## MAIN CONSTRUCTOR
         pygame.sprite.Sprite.__init__(self)
         
+        ## SETTING UP SPRITE IMAGE
         self.image = img
         self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
+        ## COLLISION RADIUS
         self.radius = int(self.rect.width * 0.85 / 2)
+        
         self.speedy = 0
+        ## BOOLEAN FOR WHETHER ENEMY IS BOSS OR NOT
         self.boss = boss
+        
         self.speedx = 5
+        
         self.size = size
+        
         self.col = col
+        
         self.rect.centerx = ((WIDTH-WIDTH_STREET)/2) + (col * WIDTH_STREET/5 - self.rect.width)
+        
         self.rect.top = random.randint(-300,-100)
+        
         self.HP = 1
+        
         if self.boss:
+        
             self.rect.centerx = ((WIDTH-WIDTH_STREET)/2) + (col * WIDTH_STREET/5 - self.rect.width)
+            
             self.rect.y = 5
+            
             self.speedx = random.randint(-5,5)
-        
-        
+            if self.speedx == 0:
+                self.speedx = random.randint(-5,5)
+            if self.speedx == 0:
+                self.speedx = random.randint(-5,5)
+
+
     def update(self):
         if self.HP <=0:
             self.kill()
